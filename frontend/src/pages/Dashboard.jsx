2fc0cd9063a0f0324538fbbd4a19d380bf8fa64a
guy0man <martinbernardbondoc@gmail.com>
2025-10-23 18:04:04 +0800

    className and datatable packages

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -20,7 +20,7 @@ function Dashboard() {
             <header className='flex items-center space-x-6 my-[5px] top-0 left-0 w-[95%] z-50'>
                 <i className="fa-solid fa-otter text-gray-900 text-2xl mx-[15px]"></i>
                 <NavigationMenu viewport={false} class='w-full'>
-                    <NavigationMenuList class='flex justify-center space-x-[15%]'>
+                    <NavigationMenuList className='flex justify-center space-x-[15%]'>
                         <NavigationMenuItem>
                             <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
                                 <NavLink to="/home">Home</NavLink>
--- frontend/src/pages/Nav_Tabs/Data_List.jsx
+++ frontend/src/pages/Nav_Tabs/Data_List.jsx
@@ -1,3 +1,33 @@
+import * as React from "react"
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  RowSelection,
+} from "@tanstack/react-table"
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Checkbox } from "@/components/ui/checkbox"
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 function DataList() {
     return (
